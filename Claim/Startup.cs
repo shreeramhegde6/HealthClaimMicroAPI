0d5b84b2e893a90164a081079b06368a4f5a9508
shreeramhegde6 <46131900+shreeramhegde6@users.noreply.github.com>
2023-03-14 16:46:54 +0530

    Add files via upload

---
+++ Claim/Startup.cs
@@ -0,0 +1,60 @@
+using Claim.Models;
+using Claim.Services;
+using Common;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Claim
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen();
+            services.AddDbContext<HealthclaimAppContext>(x => x.UseSqlServer(Configuration.GetConnectionString("HealthClaimDbConnection")));
+            services.AddScoped<IClaimService, ClaimService>();
+            services.AddConsulConfig(Configuration);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseConsul(Configuration);
+            app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
